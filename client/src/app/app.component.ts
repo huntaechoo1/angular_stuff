2a31e54f6d8985207932a88edea776b8fe0fa7cd
Hunter Choo <53629280+huntaechoo1@users.noreply.github.com>
2019-12-09 14:38:39 -0500

    modified

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { HttpService } from './http.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'client';
-  title2 = 'client2';
-  constructor(private _httpService : HttpService){
-  }
+	title = 'client';
+	title2 = 'client2';
+	constructor(private _httpService : HttpService){
+		this._httpService = _httpService;
+	}
 }
--- client/src/app/http.service.ts
+++ client/src/app/http.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,11 +9,25 @@ export class HttpService {
 
 	constructor(private _http: HttpClient) {
   		this.getTasks();
+  		this.getOneTask();
+  		this.getPokemon();
 	}
 
 	getTasks(){
 		let tempObservable = this._http.get('/tasks');
 		tempObservable.subscribe(data => console.log("Got our tasks", data));
+	}
+
+	getOneTask(){
+		let tempObservable = this._http.get('/tasks/5deaa36329d9bc41a41d9d21');
+		tempObservable.subscribe(data => console.log("Got our task", data));
+	}
+
+	getPokemon(){
+		let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+		bulbasaur.subscribe(data => console.log("Bulbasaur Data", data));
+		bulbasaur.subscribe(data => console.log(`Bulbasaur's skills are ${data["abilities"][0]["ability"]["name"]} 
+			and ${data["abilities"][1]["ability"]["name"]}`));
 	}
 
 }
